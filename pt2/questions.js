a16e615d7a83a80d797a9f1dc48b47e7f2690f52
Fisnik Lluka <Flluka@users.noreply.github.com>
2019-02-04 14:13:46 +0100

    Update GNAV PT2

--- pt2/questions.js
+++ pt2/questions.js
@@ -76,7 +76,7 @@ const questions = [
   {
     question: "Outside air temperature at FL210 is -35°C. What is the ISA deviation ",
     answer1: "8",
-    answer2: "-10",
+    answer2: "-42",
     answer3: "7",
     answer4: "-8",
     correctAnswer: 4
@@ -86,7 +86,7 @@ const questions = [
     answer1: "-56.5°C",
     answer2: "-56°C",
     answer3: "-50°C",
-    answer4: "-55°C",
+    answer4: "-41°C",
     correctAnswer: 3
   },
   {
@@ -116,7 +116,7 @@ const questions = [
   {
     
     question: "Your required track (the track you must fly) is 045(T). Your drift will be 20° starboard. Variation is +10°. What magnetic heading should you fly to give the required track? ",
-    answer1: "010° (M)",
+    answer1: "055° (M)",
     answer2: "017° (M)",
     answer3: "015° (M)",
     answer4: "020° (M)",
@@ -135,8 +135,8 @@ const questions = [
     question: "Given: Pressure Altitude 24 000ft, TAS 320kts, TAT -35, What is the approximate SAT?",
     answer1: "-46°C",
     answer2: "-44°C",
-    answer3: "-50",
-    answer4: "-56.5",
+    answer3: "-50°C",
+    answer4: "-56.5°C",
     correctAnswer: 1
   },
   {
@@ -177,6 +177,106 @@ const questions = [
     answer2: "-41",
     answer3: "-50",
     answer4: "-40",
+    correctAnswer: 2
+    
+    
+      },
+  {
+    question: "The distance between position A and B is 180NM. An aircraft departs postion A and after having travelled 60NM, its position is pinpointed 4NM left of the intended trac. Assuming no change in wind velocity, what alteration of heading must be made in order to arrive at position B?",
+    answer1: "6° left",
+    answer2: "6° right",
+    answer3: "8° left",
+    answer4: "8° right",
+    correctAnswer: 2
+    
+   },
+  {
+    question: "Which statement regarding the triangle of velocities is correct?",
+    answer1: "The three vectors must represent the same unit of time",
+    answer2: "Finner ikke alternativ",
+    answer3: "Finner ikke alternativ",
+    answer4: "Finner ikke alternativ",
+    correctAnswer: 1
+    
+   },
+  {
+    question: "Given: True HDG = 233°, TAS = 480kts, Track (T) = 240°, GS = 523kts. Calculate the W/V?",
+    answer1: "105°/75kts",
+    answer2: "110°/75kts",
+    answer3: "115°/75kts",
+    answer4: "113°/75kts",
+    correctAnswer: 2
+     },
+  {
+    question: "The TAS is 500kts, the Mach No is 0,82 What is the ambient temperature. ",
+    answer1: "-42°",
+    answer2: "-29°",
+    answer3: "-32°",
+    answer4: "-40°",
+    correctAnswer: 2
+
+    },
+  {
+    question: "Given: Runway direction 305°(M), Surface W/V 260°(M)/30kt. Calculate the crosswind component?",
+    answer1: "21kts",
+    answer2: "27kts",
+    answer3: "24kts",
+    answer4: "18kts",
+    correctAnswer: 1
+    
+    },
+  {
+    question: "Given: TAS 470kts, FL 330, Temperature ISA +6. Calculate the Mach Numder? ",
+    answer1: "0.7",
+    answer2: "0.9",
+    answer3: "0.8",
+    answer4: "0.89",
+    correctAnswer: 3
+    
+    },
+  {
+    question: "Given: True HDG = 307°, TAS = 230kt, Track (T) = 313°, GS = 210kt. Calculate W/V ",
+    answer1: "255°/25kts",
+    answer2: "257°/35kts",
+    answer3: "260°/30kts",
+    answer4: "265°/30kts",
+    correctAnswer: 3
+    
+    
+      },
+  {
+    question: "An aircraft takes off from the aerodrome with 14 000lb of fuel with SG 0.8. After 15 hours, how much fuel remains with a consumption of 850lmp Gals/hr?",
+    answer1: "41 000lb",
+    answer2: "38 000lb",
+    answer3: "35 000lb",
+    answer4: "42 000lb",
+    correctAnswer: 2
+    
+  },
+  {
+    question: "Given: TAS = 485kt, OAT = ISA +10°C, FL 410, Calculate the Mach Number?",
+    answer1: "0.85",
+    answer2: "0.90",
+    answer3: "0.825",
+    answer4: "0.87",
+    correctAnswer: 3
+    
+    },
+  {
+    question: "The weight in pounds of litres of fuel at SG 0.79 is:",
+    answer1: "11400",
+    answer2: "12400",
+    answer3: "10400",
+    answer4: "9700",
+    correctAnswer: 1
+        
+    },
+  {
+    question: "Where will the wind vector be drawn in a triangle of velocities?",
+    answer1: "From the beginning of the ground vector to the end of the air vector",
+    answer2: "From the end of the air vector to the end of the ground vector",
+    answer3: "From the beginning of the air vector to the end of the ground vector",
+    answer4: "From the end of the ground vector to the end of the air vector ",
     correctAnswer: 2
     
   }
