90aaea097abf3e29c1af1d1e4fa087b3218ff383
Fisnik Lluka <cicnik@hotmail.com>
2019-02-05 17:25:04 +0100

    Question fix

--- pt2/questions.js
+++ pt2/questions.js
@@ -1,280 +1,284 @@
 const questions = [
+  const questions = [
   {
-    question:
-      "The radius of the Earth is approximately:",
-    answer1: "40 000km",
-    answer2: "6 350km ",
-    answer3: "22 000km",
-    answer4: "20 000km",
+    question:"Given: True Heading = 310°, TAS = 200kt, GS = 176kt, Drift Angle 7° right. Calculate the W/V?",
+    answer1: "270°/33kts",
+    answer2: "360°/33kts",
+    answer3: "090°/33kts",
+    answer4: "180°/33kts",
+    correctAnswer: 1
+  },
+  {
+    question:"Pressure Altitude 41000ft, Temperature -70°C, CAS = 270kts. What is TAS?",
+    answer1: "472kts",
+    answer2: "496kts",
+    answer3: "473kts",
+    answer4: "497",
+    correctAnswer: 1
+
+  },
+  {
+    question: "Given: Distamce A to B = 120NM. After 60NM aircraft is 4NM to the right of couse. What heading alteration should be made in order to arrive at point B?",
+    answer1: "8° right",
+    answer2: "8° left",
+    answer3: "12° right",
+    answer4: "4° left",
     correctAnswer: 2
   },
   {
-    question:
-      "An aircraft at latitude 02°20´N tracks 180°T for 685 kilometres. What is its latitude at the end of the flight",
-    answer1: "08°29´N",
-    answer2: "06°01´N",
-    answer3: "02°45´S",
-    answer4: "03°50´S",
-    correctAnswer: 1
-  },
-  {
-    question: "What is the initial great circle track from Porto Alegre, Brazil (29°12´S 051°00´W) to durban, South africa (29°12´S 031°00´E)?",
-    answer1: "110°T",
-    answer2: "130°T",
-    answer3: "105°T",
-    answer4: "115°T",
-    correctAnswer: 1
-  },
-  {
-    question: "A line drawn on a chart which joins all points where the value of magnetic variation is zero is called an:",
-    answer1: "Isogonal",
-    answer2: "aclinic line",
-    answer3: "agonic line",
-    answer4: "isotach",
+    question: "A distance of 436 nautical miles is equivalent to:",
+    answer1: "500 statute miles",
+    answer2: "501 km",
+    answer3: "501 statute miles",
+    answer4: "501 metres",
     correctAnswer: 3
   },
   {
-    question:
-      "What is the initial great circle track from sydney, Australia (30S150E) to Bueons Aires, Argentina (30W030W)?",
-    answer1: "180°T",
-    answer2: "150°T",
-    answer3: "190°T",
-    answer4: "178°T",
-    correctAnswer: 1
-  },
-  {
-    question:
-      "An approximate equation for calculation conversion angle is: ",
-    answer1: "CA=0.5 x dlong x sin(mean lat)",
-    answer2: "CA=dlong x sin(mean lat) x sin(long)",
-    answer3: "CA=(dlong-dlat) x 0.5",
-    answer4: "CA=0.5 x dlat x sin(mean lat)",
-    correctAnswer: 1
-  },
-  {
-    question: "When flying on a westerly great circle track in the Southern hemisphere you will:",
-    answer1: "fly a spiral and finally end ut at the south pole",
-    answer2: "experience an increase in the value of track",
-    answer3: "aleays have the rhumb line track between the departure point and the destination to the left of your great circle track",
-    answer4: "experience a decrease in the value of true track",
-    correctAnswer: 2
-  },
-  {
-    question:
-      "What is the maximum possible value of Dip Angle",
-    answer1: "0°",
-    answer2: "90°",
-    answer3: "350°",
-    answer3: "170°",
-    answer4: "89°",
-    correctAnswer: 2
-  },
-  {
-    question:
-      "In the northern hemisphere, the true direction of an westerly Great Circle will",
-    answer1: "Increase",
-    answer2: "Decrease",
-    answer3: "Be the same",
-    answer4: "all the answers are correct",
-    correctAnswer: 2
-  },
-  {
-    question: "You start from P (70°00.0´N 015°00.0´E) and fly westward along the parallel of latitude for 2 hours at ground speed 220kt. Whar is your longitude after two hoursus flight?",
-    answer1: "006°26.0´W",
-    answer2: "006°44.0´W",
-    answer3: "006°78.0´W",
-    answer4: "006°26.0´E",
-    correctAnswer: 1
-  },
-  {
-    question: "Seasons are due to the:",
-    answer1: "Earths elliptical orbit around the sun",
-    answer2: "inclination of the polar axis with the ecliptic plane",
-    answer3: "Earths rotation on its polar axis",
-    answer4: "variable distance between Earth and Southern",
-    correctAnswer: 2
-  },
-  {
-    question: "The direction of the Earths rotation in its axis is such that",
-    answer1: "observed from the point above the North Pole, the rotation is counterclockwise",
-    answer2: "an observer on the surface of the Earth always will face West when observing sunrise",
-    answer3: "any point on the surface of the Earth will move eastward",
-    answer4: "any point on the surface of the Earth will move westward",
-    correctAnswer: 1
-  },
-  {
-    question: "Meridians are:",
-    answer1: "parallel, equally spaced, vertically straight lines",
-    answer2: "inclined, equally spaced straight lines that meet at the nearer pole",
-    answer3: "parallel, unequally spaced, vertical straight lines",
-    answer4: "inclined, unequally spaced, curved lines that meet at the nearer pole",
-    correctAnswer: 1
-  },
-  {
-    question: "Keplers second law states that",
-    answer1: "the radius vector sun-earth sweeps out equal areas in equal time",
-    answer2: "the length of the radius vector sun-earth is directly proportional to the square root of its angular speed",
-    answer3: "the radius vector sun-earth moves at constant angular speed",
-    answer4: "the area swept out by the radius vector sun-earth per hour increases with increasing lenght of the radius vector",
-    correctAnswer: 1
-  },
-  {
-    question: "The convergency of meridians",
-    answer1: "is the angular difference between meridians",
-    answer2: "is the distance between the meridians in degrees, minutes and seconds",
-    answer3: "is independent of latitude and longitude",
-    answer4: "is greater using rhumb line track than using greater circle",
-    correctAnswer: 1
-  },
-  {
-    question:
-      "Parallels of latitude, except the equator are:",
-    answer1: "both rhumb lines and great circles",
-    answer2: "Great circles",
-    answer3: "rhumb lines",
-    answer4: "are neither rhumb lines nor great circles",
+    question:"Your heading is 295°T. Your drift is 15° port(left). Variation is 5°W. Deviation is 2°E. What is your compass track?",
+    answer1: "263°(C)",
+    answer2: "313°(C)",
+    answer3: "283°(C)",
+    answer4: "300°(C)",
     correctAnswer: 3
   },
   {
+    question:"Given: IAS 240kt, FL 150, OAT -20. What is the TAS?",
+    answer1: "298kt",
+    answer2: "280kt",
+    answer3: "352kt",
+    answer4: "260kt",
+    correctAnswer: 1
+  },
+  {
+    question: "Given: TAS = 200kt, Track (T) = 110°, W/V = 015/40kt. Calculate the HDG (°T) and GS?",
+    answer1: "121 - 207kts",
+    answer2: "099 - 199kts",
+    answer3: "097 - 201kts",
+    answer4: "121 - 199kts ",
+    correctAnswer: 2
+  },
+  {
+    question: "Temperature at FL410 is -45. What is the ISA deviation",
+    answer1: "11.5",
+    answer2: "12",
+    answer3: "10",
+    answer4: "5",
+    correctAnswer: 1
+  },
+  {
     question:
-      "The compression factor of the earth is approximately",
-    answer1: "is so small that it may be ignored when making ordinary maps and chart",
-    answer2: "is about 1:300",
-    answer3: "makes the difference between the polar diameter and the equatorial diamter about 22NM",
-    answer4: "All Answers are correct",
+      "Given: FL350. Mach 0.80, OAT -55°C. Calculate the values for TAS and local speed of sound (LSS)",
+    answer1: "460kt, LSS 296kt",
+    answer2: "237kt, LSS 296kt",
+    answer3: "461kt, LSS 576kt",
+    answer4: "490kt, LSS 460kt",
+    correctAnswer: 3
+  },
+  {
+    question: "Outside air temperature at FL210 is -35°C. What is the ISA deviation ",
+    answer1: "8",
+    answer2: "-42",
+    answer3: "7",
+    answer4: "-8",
     correctAnswer: 4
   },
   {
-    question: "Isogonals are lines of equal",
-    answer1: "Areas of high level clear air Turbulence",
-    answer2: "Jet streams",
-    answer3: "Stratospheric inversions",
-    answer4: "Mountain waves",
+    question: "Indicated Outside Air Temperature -30°C, TAS 438kts. What is the corrected Outside Air Temperature (COAT)? ",
+    answer1: "-56.5°C",
+    answer2: "-56°C",
+    answer3: "-50°C",
+    answer4: "-41°C",
+    correctAnswer: 3
+  },
+  {
+    question: "Pressure Altitude 30 000ft, Temperature -40°C, CAS = 260kts. What is TAS?",
+    answer1: "415kt",
+    answer2: "413kt ",
+    answer3: "421kt",
+    answer4: "412kt",
     correctAnswer: 4
   },
   {
-    question:"Consider the following statements on departure",
-    answer1: "as the difference of longitude increases, the departure is constant if the latitude is constant",
-    answer2: "as the latitude increases, the departure between two meridians decreases",
-    answer3: "departure may be calculated using the equation: departure = Sin(Lat) x Sin(Long)",
-    answer4: "departure is independent of difference of longitude",
+    question: "For a landing on runway 23 (227° Magnetic) surface W/V reported by the ATIS is 180/30kts. VAR is 13°E. Calculate the cross wind component",
+    answer1: "20kts",
+    answer2: "22kts",
+    answer3: "26kts",
+    answer4: "15kts",
     correctAnswer: 2
   },
   {
-    question:
-      "Consider the following statements on rhumb lines",
-    answer1: "most rhumb lines will run as spirals from the one pole to another",
-    answer2: "a rhumb line will never cross a great circle",
-    answer3: "a rhumb line and a great circle will never have the same true direction for some distance",
-    answer4: "the true direction of a rhumb line on northern hemisphere will increase in true direction, while on southern hemisphere it will decrease",
-    correctAnswer: 1
+    question: "Given: TAS = 90kt, HDG(T) = 355°, W/V = 120/20kt. Calculate track (°T) and GS?",
+    answer1: "006 - 95kts",
+    answer2: "345 - 102kts",
+    answer3: "358 - 101kts",
+    answer4: "359 - 102kts",
+    correctAnswer: 2
   },
   {
-    question:
-      "What is the change of longitude between A(45°00´N 163°14´E) and B(31°33´N 157°02E",
-    answer1: "6°12´W",
-    answer2: "13°27´W",
-    answer3: " 7°13´W",
-    answer4: "6°12´E",
-    correctAnswer: 1
-  },
-  {
-    question:
-      "Postion A is (31°00´S, 176°17´W) rhumb line track (T) from A to B is 270°. Initial great circle track (T) from A to B is 266.2°. The approximate position of B is:",
-    answer1: "(31°00´S, 161°32´W)",
-    answer2: "(31°00´S, 173°24´E)",
-    answer3: "(31°00´S, 168°58´E)",
-    answer4: "(31°00´S, 173°24´W)",
+
+    question: "Your required track (the track you must fly) is 045(T). Your drift will be 20° starboard. Variation is +10°. What magnetic heading should you fly to give the required track? ",
+    answer1: "055° (M)",
+    answer2: "017° (M)",
+    answer3: "015° (M)",
+    answer4: "020° (M)",
     correctAnswer: 3
   },
   {
     question:
-      "Consider the following statements on the properties of a great circle:",
-    answer1: "the great circle will maintain their initial true direction",
-    answer2: "the parallels of latitudes are all great circles",
-    answer3: "the great circle running through two positions on the surface of the Earth, is the shortest distance between these two postions",
-    answer4: "all answers are Correct",
-    correctAnswer: 3
+      "Given: TAS = 485kt, HDG(T) = 168°, W/V = 130/75kt, Calculate the Track (°T) and GS?",
+    answer1: "175 - 432kts",
+    answer2: "173 - 424kts",
+    answer3: "175 - 420kts",
+    answer4: "174 - 428kts",
+    correctAnswer: 4
   },
   {
-    question:
-      "An aircraft is on a heading of 111°C, Variation is 7E and Deviation 4E. What is the aircrafts True Heading?",
-    answer1: "122°T",
-    answer2: "114°T",
-    answer3: "120°T",
-    answer4: "118°T",
+    question: "Given: Pressure Altitude 24 000ft, TAS 320kts, TAT -35, What is the approximate SAT?",
+    answer1: "-46°C",
+    answer2: "-44°C",
+    answer3: "-50°C",
+    answer4: "-56.5°C",
     correctAnswer: 1
   },
   {
-    question:
-      "Conversion angle is: (most correct answer)",
-    answer1: "The angular difference between the rhumb line and the great circle between two positions, measured at any of the postions ",
-    answer2: "the difference between the rhumb line and the great circle directions",
-    answer3: "the angle used to convert from true to compass directions",
-    answer4: "the angle at which speech from another person enters the ear",
-    correctAnswer: 1
-  },
-  {
-    question:
-      "A straight line drawn on a chart measures 4.63cm and represents 150NM. The chart scale is:",
-    answer1: "1:6 000 000",
-    answer2: "1:3 000 000",
-    answer3: "1:1 000 000",
-    answer4: "1:5 000 000",
-    correctAnswer: 1
-  },
-  {
-    question:
-      "An aircrafft is on a heading of 354°C. Variation is 2W and Deviation 8E. What is the aircrafts True Heading?",
-    answer1: "0°T",
-    answer2: "004°T",
-    answer3: "010°T",
-    answer4: "030°T",
-    correctAnswer: 1
-  },
-  {
-    question:
-      "What is the aprroximate latitude of the Antarctic Circle?",
-    answer1: "66 1/2°S",
-    answer2: "23°1/2N",
-    answer3: "88 1/2°W",
-    answer4: "66°1/2E",
-    correctAnswer: 1
-  },
-  {
-    question:
-      "The term departure used in navigation also has the following meaning:?",
-    answer1: "distance in direction east/west, given in nautical miles",
-    answer2: "distance North/South",
-    answer3: "angular distance along meridian",
-    answer4: "angular distance along a parallel of latitude",
-    correctAnswer: 1
-  },
-  {
-    question:
-      "What is the distance in kilometres from 49°S 180°E/W to 58°S 180°E/W",
-    answer1: "1 000km ",
-    answer2: "540km",
-    answer3: "500km",
-    answer4: "800km",
-    correctAnswer: 1
-  },
-  {
-    question:
-      "The great circle distance between position A(59°34.1´N 008°08.4´E) and B (30°25.9´N 171°51.6´W) is:",
-    answer1: "10 800NM",
-    answer2: "5 400NM",
-    answer3: "4 500NM",
-    answer4: "5 500NM",
+    question:"What is 48 000KG in pound is approximately",
+    answer1: "21 818lb",
+    answer2: "105 600lb",
+    answer3: "110 700lb",
+    answer4: "56 300lb",
     correctAnswer: 2
   },
   {
-    question:
-      "The maximum difference between geocentric and geodetic latitude occurs at about",
-    answer1: "45° North and South",
-    answer2: "90° North and South",
-    answer3: "60° North and South",
-    answer4: "0° North and South (Equator)",
+    question: "Given: GS = 105kt. Distance from A to B = 103NM. What is the time from A to B? ",
+    answer1: "01hr 01min",
+    answer2: "00hr 57min",
+    answer3: "00hr 58min",
+    answer4: "00hr 59min",
+    correctAnswer: 4
+  },
+  {
+    question: "In a triangle of velocities a vector drawn with two arrows shows:",
+    answer1: "TAS and Heading ",
+    answer2: "Groundspeed and Track",
+    answer3: " IAS and Heading",
+    answer4: "CAS and Track",
+    correctAnswer: 2
+  },
+  {
+    question: "Given: TAS = 227kt, Required Track(T) = 316°, W/V = 205/15kt. Calculate the HDG(°T) and GS?",
+    answer1: "319 - 235kts",
+    answer2: "311 - 230kts",
+    answer3: "310 - 233kts",
+    answer4: "313 - 232kts",
+    correctAnswer: 4
+  },
+  {
+    question: " The TAS is 415kts, the Mach No. is 0.7. What is the ambient temperature?",
+    answer1: "-44",
+    answer2: "-41",
+    answer3: "-50",
+    answer4: "-40",
+    correctAnswer: 2
+
+
+      },
+  {
+    question: "The distance between position A and B is 180NM. An aircraft departs postion A and after having travelled 60NM, its position is pinpointed 4NM left of the intended trac. Assuming no change in wind velocity, what alteration of heading must be made in order to arrive at position B?",
+    answer1: "6° left",
+    answer2: "6° right",
+    answer3: "8° left",
+    answer4: "8° right",
+    correctAnswer: 2
+
+   },
+  {
+    question: "Which statement regarding the triangle of velocities is correct?",
+    answer1: "The three vectors must represent the same unit of time",
+    answer2: "Finner ikke alternativ",
+    answer3: "Finner ikke alternativ",
+    answer4: "Finner ikke alternativ",
     correctAnswer: 1
+
+   },
+  {
+    question: "Given: True HDG = 233°, TAS = 480kts, Track (T) = 240°, GS = 523kts. Calculate the W/V?",
+    answer1: "105°/75kts",
+    answer2: "110°/75kts",
+    answer3: "115°/75kts",
+    answer4: "113°/75kts",
+    correctAnswer: 2
+     },
+  {
+    question: "The TAS is 500kts, the Mach No is 0,82 What is the ambient temperature. ",
+    answer1: "-42°",
+    answer2: "-29°",
+    answer3: "-32°",
+    answer4: "-40°",
+    correctAnswer: 2
+
+    },
+  {
+    question: "Given: Runway direction 305°(M), Surface W/V 260°(M)/30kt. Calculate the crosswind component?",
+    answer1: "21kts",
+    answer2: "27kts",
+    answer3: "24kts",
+    answer4: "18kts",
+    correctAnswer: 1
+
+    },
+  {
+    question: "Given: TAS 470kts, FL 330, Temperature ISA +6. Calculate the Mach Numder? ",
+    answer1: "0.7",
+    answer2: "0.9",
+    answer3: "0.8",
+    answer4: "0.89",
+    correctAnswer: 3
+
+    },
+  {
+    question: "Given: True HDG = 307°, TAS = 230kt, Track (T) = 313°, GS = 210kt. Calculate W/V ",
+    answer1: "255°/25kts",
+    answer2: "257°/35kts",
+    answer3: "260°/30kts",
+    answer4: "265°/30kts",
+    correctAnswer: 3
+
+
+      },
+  {
+    question: "An aircraft takes off from the aerodrome with 14 000lb of fuel with SG 0.8. After 15 hours, how much fuel remains with a consumption of 850lmp Gals/hr?",
+    answer1: "41 000lb",
+    answer2: "38 000lb",
+    answer3: "35 000lb",
+    answer4: "42 000lb",
+    correctAnswer: 2
+
+  },
+  {
+    question: "Given: TAS = 485kt, OAT = ISA +10°C, FL 410, Calculate the Mach Number?",
+    answer1: "0.85",
+    answer2: "0.90",
+    answer3: "0.825",
+    answer4: "0.87",
+    correctAnswer: 3
+
+    },
+  {
+    question: "The weight in pounds of 6550 litres of fuel at SG 0.79 is:",
+    answer1: "11400",
+    answer2: "12400",
+    answer3: "10400",
+    answer4: "9700",
+    correctAnswer: 1
+
+    },
+  {
+    question: "Where will the wind vector be drawn in a triangle of velocities?",
+    answer1: "From the beginning of the ground vector to the end of the air vector",
+    answer2: "From the end of the air vector to the end of the ground vector",
+    answer3: "From the beginning of the air vector to the end of the ground vector",
+    answer4: "From the end of the ground vector to the end of the air vector ",
+    correctAnswer: 2
+
   }
 ];
