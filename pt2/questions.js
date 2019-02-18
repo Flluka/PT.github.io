dfd285f756705be628586a153ab8162527e50832
Fisnik Lluka <Flluka@users.noreply.github.com>
2019-02-18 21:59:10 +0100

    GNAV 3

--- pt2/questions.js
+++ pt2/questions.js
@@ -1,152 +1,151 @@
 const questions = [
 
   {
-    question:"Given: True Heading = 310°, TAS = 200kt, GS = 176kt, Drift Angle 7° right. Calculate the W/V?",
-    answer1: "270°/33kts",
-    answer2: "360°/33kts",
-    answer3: "090°/33kts",
-    answer4: "180°/33kts",
+    question:"The distance between positions C and D is 90NM. An aircraft departs position C and after having travelled 30NM, its position is pinpointed 3NM right og the intended track. Assuming no change in wind velocity, what heading alteration must be made in order to arrive at position D? ",
+    answer1: "9 Left",
+    answer2: "9 Right ",
+    answer3: "2 Left",
+    answer4: "6 Left",
     correctAnswer: 1
   },
   {
-    question:"Pressure Altitude 41000ft, Temperature -70°C, CAS = 270kts. What is TAS?",
-    answer1: "472kts",
-    answer2: "496kts",
-    answer3: "473kts",
-    answer4: "497",
-    correctAnswer: 1
+    question:"In producing chart projections, the following projection surfaces may be used: ",
+    answer1: "Plane, Shpere, Cone",
+    answer2: "Plane, Cylinder, Cone",
+    answer3: "Cylinder, Sphere, Plane",
+    answer4: "Parabola, Cone, Plane, Cylinder",
+    correctAnswer: 2
 
   },
   {
-    question: "Given: Distamce A to B = 120NM. After 60NM aircraft is 4NM to the right of couse. What heading alteration should be made in order to arrive at point B?",
-    answer1: "8° right",
-    answer2: "8° left",
-    answer3: "12° right",
-    answer4: "4° left",
-    correctAnswer: 2
-  },
-  {
-    question: "A distance of 436 nautical miles is equivalent to:",
-    answer1: "500 statute miles",
-    answer2: "501 km",
-    answer3: "501 statute miles",
-    answer4: "501 metres",
-    correctAnswer: 3
-  },
-  {
-    question:"Your heading is 295°T. Your drift is 15° port(left). Variation is 5°W. Deviation is 2°E. What is your compass track?",
-    answer1: "263°(C)",
-    answer2: "313°(C)",
-    answer3: "283°(C)",
-    answer4: "300°(C)",
-    correctAnswer: 3
-  },
-  {
-    question:"Given: IAS 240kt, FL 150, OAT -20. What is the TAS?",
-    answer1: "298kt",
-    answer2: "280kt",
-    answer3: "352kt",
-    answer4: "260kt",
-    correctAnswer: 1
-  },
-  {
-    question: "Given: TAS = 200kt, Track (T) = 110°, W/V = 015/40kt. Calculate the HDG (°T) and GS?",
-    answer1: "121 - 207kts",
-    answer2: "099 - 199kts",
-    answer3: "097 - 201kts",
-    answer4: "121 - 199kts ",
-    correctAnswer: 2
-  },
-  {
-    question: "Temperature at FL410 is -45. What is the ISA deviation",
-    answer1: "11.5",
-    answer2: "12",
-    answer3: "10",
-    answer4: "5",
-    correctAnswer: 1
-  },
-  {
-    question:
-      "Given: FL350. Mach 0.80, OAT -55°C. Calculate the values for TAS and local speed of sound (LSS)",
-    answer1: "460kt, LSS 296kt",
-    answer2: "237kt, LSS 296kt",
-    answer3: "461kt, LSS 576kt",
-    answer4: "490kt, LSS 460kt",
-    correctAnswer: 3
-  },
-  {
-    question: "Outside air temperature at FL210 is -35°C. What is the ISA deviation ",
-    answer1: "8",
-    answer2: "-42",
-    answer3: "7",
-    answer4: "-8",
+    question: "On a direct Mercator chart at latitude of 45°N, a certain length represents a distance of 90NM on the earth. The same length on the chart will represent on the earth, at latitude 30°N, a distance of:",
+    answer1: "45NM",
+    answer2: "73.5NM",
+    answer3: "78NM",
+    answer4: "110NM",
     correctAnswer: 4
   },
   {
-    question: "Total Air temperature (TAT) is -30°C, TAS 438kts. What is the static air temperature (SAT)? ",
-    answer1: "-56.5°C",
-    answer2: "-56°C",
-    answer3: "-50°C",
-    answer4: "-41°C",
-    correctAnswer: 3
-  },
-  {
-    question: "Pressure Altitude 30 000ft, Temperature -40°C, CAS = 260kts. What is TAS?",
-    answer1: "415kt",
-    answer2: "414kt ",
-    answer3: "421kt",
-    answer4: "412kt",
-    correctAnswer: 4
-  },
-  {
-    question: "For a landing on runway 23 (227° Magnetic) surface W/V reported by the ATIS is 180/30kts. VAR is 13°E. Calculate the cross wind component",
-    answer1: "20kts",
-    answer2: "22kts",
-    answer3: "26kts",
-    answer4: "15kts",
+    question: "The countries having a standard time slow on UTC:",
+    answer1: "Will often have an earlier standard date than the UTC date",
+    answer2: "Will generally be located at western longitudes",
+    answer3: "Will often experience sunrise earlier than the sunrise occurs at the Greenwhich meridian",
+    answer4: "Will generally be located at eastern longitudes",
     correctAnswer: 2
   },
   {
-    question: "Given: TAS = 90kt, HDG(T) = 355°, W/V = 120/20kt. Calculate track (°T) and GS?",
-    answer1: "006 - 95kts",
-    answer2: "345 - 102kts",
-    answer3: "358 - 101kts",
-    answer4: "359 - 102kts",
+    question:"On a Polar Stereographic chart, chart convergence is:",
+    answer1: "Correct at the Pole",
+    answer2: "Correct at the equator",
+    answer3: "Correct at the North Pole",
+    answer4: "Correct at the Sout Pole",
+    correctAnswer: 1
+  },
+  {
+    question:"For a Lambert Chart with a Parallel Of Origin at 60S, the convergence Factor is: ",
+    answer1: "1.0",
+    answer2: "0.0",
+    answer3: "0.5",
+    answer4: "0.866",
+    correctAnswer: 4
+  },
+  {
+    question: "What is the latitude of the Tropic of Cancer",
+    answer1: "23°1/2N",
+    answer2: "23°1/2S",
+    answer3: "66°1/2N",
+    answer4: "45°N/S",
+    correctAnswer: 1
+  },
+  {
+    question: "Given: Magnetic Heading 165°, Drift angle 10° right, Relative bearing of NDB 320°(R). Variation 6°E. Deviation +5°. What is the true bearing of the aircraft measured from the NDB (QTE)",
+    answer1: "311°(T)",
+    answer2: "311°(M)",
+    answer3: "300°(T)",
+    answer4: "315°(T)",
+    correctAnswer: 1
+  },
+  {
+    question:"265 US - GAL equals? (specific gravity 0.80) ",
+    answer1: "862kg",
+    answer2: "803kg",
+    answer3: "895kg",
+    answer4: "940kg",
+    correctAnswer: 2
+  },
+  {
+    question: "Preparing a chart for use of grid means:",
+    answer1: "Pre - calculating grid directions for all positions marked on the chart",
+    answer2: "Mark the chart with lines and values for grid latitude and longitude",
+    answer3: "Selecting suitable grid tracks, position line and bearings",
+    answer4: "Selecting a meridian on the chart drawing line on chart, parallel to the meridian selected",
+    correctAnswer: 4
+  },
+  {
+    question: "OAT = +35°C, Pressure Alt= 5000 feet, What is true alt?",
+    answer1: "4550feet",
+    answer2: "5550feet",
+    answer3: "4290feet",
+    answer4: "5320feet",
+    correctAnswer: 2
+  },
+  {
+    question: "What do you understand by the term whiteout?",
+    answer1: "Flight condition when you suddenly enter a cloud and everything gets white outside the cockpit windows",
+    answer2: "When the terrain is covered with snow and the horizon blend with the sky, visual determination of height becoming difficult",
+    answer3: "Flying in heavy snow",
+    answer4: "Taking off from snow covered lake",
+    correctAnswer: 2
+  },
+  {
+    question: "An aircraft at FL290 is required to commence descent when 50NM from a VOR and to cross that VOR at FL80. Mean GS during descent is 271kts. What is the minimum descent required?",
+    answer1: "1700ft/min",
+    answer2: "2000ft/min",
+    answer3: "1900ft/min",
+    answer4: "1800ft/min",
+    correctAnswer: 3
+  },
+  {
+    question: "An aircraft is maintaining a 5.2% gradient at 7NM from the runway, on a flat terrain its height is approximately:",
+    answer1: "680ft",
+    answer2: "2210ft",
+    answer3: "1890ft",
+    answer4: "3640ft",
     correctAnswer: 2
   },
   {
 
-    question: "Your required track (the track you must fly) is 045(T). Your drift will be 20° starboard. Variation is +10°. What magnetic heading should you fly to give the required track? ",
-    answer1: "055° (M)",
-    answer2: "017° (M)",
-    answer3: "015° (M)",
-    answer4: "020° (M)",
-    correctAnswer: 3
+    question: "Using Chart E(LO)1/2. SHA VOR (52°43´N 08°53´W) DME 41NM. CRK VOR (51°50´N 008°29´W) DME 30NM. The aircraft is on a west southerly heading and both DMEs decreasing. What is the position of the aircraft?",
+    answer1: "52°15´N 008°06´W",
+    answer2: "52°05´N 009°15´W",
+    answer3: "52°15`N 009°15´W",
+    answer4: "52°25´N 008°10´W",
+      correctAnswer: 1
   },
   {
     question:
-      "Given: TAS = 485kt, HDG(T) = 168°, W/V = 130/75kt, Calculate the Track (°T) and GS?",
-    answer1: "175 - 432kts",
-    answer2: "173 - 424kts",
-    answer3: "175 - 420kts",
-    answer4: "174 - 428kts",
-    correctAnswer: 4
+      "An aircraft tracks radial 200 inbound to a VOR station with a Magnetic Heading(MH) of 010°. After being overhead the VOR station the aircraft tracks radial 090° outbound with a MH of 080°. The TAS is 240kt and the magnetic variation in the area is 5°W. What is the wind vector?(°T) ",
+    answer1: "300°/50kts",
+    answer2: "330°/50kts",
+    answer3: "320°/50kts",
+    answer4: "310°/60kts",
+    correctAnswer: 3
   },
   {
-    question: "Given: Pressure Altitude 24 000ft, TAS 320kts, TAT -35, What is the approximate SAT?",
-    answer1: "-46°C",
-    answer2: "-44°C",
-    answer3: "-50°C",
-    answer4: "-56.5°C",
-    correctAnswer: 1
-  },
-  {
-    question:"What is 48 000KG in pound is approximately",
-    answer1: "21 818lb",
-    answer2: "105 600lb",
-    answer3: "110 700lb",
-    answer4: "56 300lb",
+    question: "An aircraft is at position (53°N, 006°W) and has a landmark at position (52°47´N 004°45´W), with a relative bearing of 060°. Given: Compass Heading = 051°. Variation = 16°W. Deviation = 2°E. What is the true bearing of the position line to be plotted from the landmark to the aircraft on a Lambert chart with standard parallels at 37°N and 65°N?",
+    answer1: "276°",
+    answer2: "278°",
+    answer3: "250°",
+    answer4: "277°",
     correctAnswer: 2
+  },
+  {
+    question: "How much time does it take for the mean sun to move from meridian 145°15´E to meridian 023°45´W?",
+    answer1: "08 hours 16 minutes",
+    answer2: "9 hours 41 minutes",
+    answer3: "11 hours 16 minutes",
+    answer4: "06 hours 15 minutes",
+    correctAnswer: 3
   },
   {
     question: "Given: GS = 105kt. Distance from A to B = 103NM. What is the time from A to B? ",
@@ -157,55 +156,55 @@ const questions = [
     correctAnswer: 4
   },
   {
-    question: "In a triangle of velocities a vector drawn with two arrows shows:",
-    answer1: "TAS and Heading ",
-    answer2: "Groundspeed and Track",
-    answer3: " IAS and Heading",
-    answer4: "Wind direction and speed",
+    question: "If the pressure altitude is 5000ft and the ambient outside air temperature is ISA +20°C, what is density altitude? :",
+    answer1: "8620 feet",
+    answer2: "7400 feet",
+    answer3: "6980 feet",
+    answer4: "7750 feet",
     correctAnswer: 2
   },
   {
-    question: "Given: TAS = 227kt, Required Track(T) = 316°, W/V = 205/15kt. Calculate the HDG(°T) and GS?",
-    answer1: "319 - 235kts",
-    answer2: "311 - 230kts",
-    answer3: "310 - 233kts",
-    answer4: "313 - 232kts",
-    correctAnswer: 4
+    question: "On a lambert chart a straight line track crosses a meridian with a direction of 043°T and a second meridian with a direction of 055°T. If the constant of the cone is 0.75 the difference in logitude between the meridian is: ",
+    answer1: "16°",
+    answer2: "19°",
+    answer3: "14°",
+    answer4: "12°",
+    correctAnswer: 1
   },
   {
-    question: " The TAS is 415kts, the Mach No. is 0.7. What is the ambient temperature?",
-    answer1: "-44",
-    answer2: "-41",
-    answer3: "-50",
-    answer4: "-40",
+    question: "if the grivation is 6°W and the variation is 8°W, what is the value of grid convergence?",
+    answer1: "2°W",
+    answer2: "2°E",
+    answer3: "4°E",
+    answer4: "6°E",
     correctAnswer: 2
 
       },
   {
-    question: "The distance between position A and B is 180NM. An aircraft departs postion A and after having travelled 60NM, its position is pinpointed 4NM left of the intended trac. Assuming no change in wind velocity, what alteration of heading must be made in order to arrive at position B?",
-    answer1: "2° left",
-    answer2: "6° right",
-    answer3: "4° right",
-    answer4: "8° right",
-    correctAnswer: 2
-
-   },
-  {
-    question: "Which statement regarding the triangle of velocities is correct?",
-    answer1: "The three vectors must represent the same unit of time",
-    answer2: "Finner ikke alternativ",
-    answer3: "Finner ikke alternativ",
-    answer4: "Finner ikke alternativ",
+    question: "On a Lamberts Conformal Chart, chart scale is correct?"
+    answer1: "At the standard parallels",
+    answer2: "At the Parallel of Origin",
+    answer3: "Along the prime meridian",
+    answer4: "In the area between the standard parallels",
     correctAnswer: 1
 
    },
   {
-    question: "Given: True HDG = 233°, TAS = 480kts, Track (T) = 240°, GS = 523kts. Calculate the W/V?",
-    answer1: "105°/75kts",
-    answer2: "110°/75kts",
-    answer3: "115°/75kts",
-    answer4: "113°/75kts",
-    correctAnswer: 2
+    question: "Grid Convergence:",
+    answer1: "Named easterly when Grid North appears to be east of true North ",
+    answer2: "Indenpendent of lonigtude, depending only of latitude",
+    answer3: "1 on equatorial Mercator chart",
+    answer4: "the difference in direction between Grid North and True North",
+    correctAnswer: 4
+
+   },
+  {
+    question: "A straight line drawn on a chart measures 4.63cm and represents 150NM. The chart scale is: ",
+    answer1: "1:6000000",
+    answer2: "1:1000000",
+    answer3: "1:3000000",
+    answer4: "1:5000000",
+    correctAnswer: 1
      },
   {
     question: "The TAS is 500kts, the Mach No is 0,82 What is the ambient temperature. ",
